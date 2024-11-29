743433f1b2ca4c4ef77666b5f43297bcc3440681
Amore G <aviroen@gmail.com>
2024-11-28 21:03:17 -0800

    Update BBEG.cs
    Put in most movement but still ignored phases, leaving that for BehaviorAtGameTick

--- BBEG.cs
+++ BBEG.cs
@@ -1,4 +1,4 @@
-﻿using StardewValley;
+using StardewValley;
 using Microsoft.Xna.Framework;
 using StardewValley.Monsters;
 using Microsoft.Xna.Framework.Graphics;
@@ -27,14 +27,20 @@ public class BBEG : Monster
     }
     [XmlIgnore]
     public List<FarmerSprite.AnimationFrame> walkingAnimation = new List<FarmerSprite.AnimationFrame>();
+
     [XmlIgnore]
     public List<FarmerSprite.AnimationFrame> runningAnimation = new List<FarmerSprite.AnimationFrame>();
+
     [XmlIgnore]
     public List<FarmerSprite.AnimationFrame> sprintingAnimation = new List<FarmerSprite.AnimationFrame>();
+
     [XmlIgnore]
-    public List<FarmerSprite.AnimationFrame> locallyPlayingAnimation;
+    public List<FarmerSprite.AnimationFrame>? locallyPlayingAnimation;
 
     public float randomStackOffset;
+
+    [XmlIgnore]
+    public int walkSpeed;
 
     [XmlIgnore]
     public NetEvent1Field<Vector2, NetVector2> attackedEvent = new NetEvent1Field<Vector2, NetVector2>();
@@ -47,6 +53,9 @@ public class BBEG : Monster
 
     [XmlIgnore]
     public bool approachFarmer;
+
+    [XmlIgnore]
+    public Farmer? targettedFarmer;
 
     [XmlIgnore]
     public Vector2 velocity = Vector2.Zero;
@@ -78,6 +87,9 @@ public class BBEG : Monster
         base.Slipperiness = 4;
         base.flip = Game1.random.NextDouble() < 0.45;
         base.HideShadow = false;
+        this.Sprite.SpriteWidth = 64;
+        this.Sprite.SpriteHeight = 128;
+        this.stateTimer = Utility.RandomFloat(3f, 5f);
     }
     public BBEG(Vector2 position, GameLocation location)
         : base("Aviroen.Voidsent_Konryn", position)
@@ -161,6 +173,13 @@ public class BBEG : Monster
         }
         return false;
     }
+    public virtual void SetRandomMovement()
+    {
+        this.velocity = new Vector2((Game1.random.Next(2) != 1) ? 1 : (-1), (Game1.random.Next(2) != 1) ? 1 : (-1));
+    }
+    protected override void updateMonsterSlaveAnimation(GameTime time)
+    {
+    }
     public virtual void OnAttacked(Vector2 trajectory)
     {
         if (Game1.IsMasterGame)
@@ -213,5 +232,128 @@ public class BBEG : Monster
             }
         }
         return actualDamage;
+    }
+    public virtual bool TargetInRange()
+    {
+        if (this.targettedFarmer == null)
+        {
+            return false;
+        }
+        if (Math.Abs(this.targettedFarmer.Position.X - base.Position.X) <= 640f && Math.Abs(this.targettedFarmer.Position.Y - base.Position.Y) <= 640f)
+        {
+            return true;
+        }
+        return false;
+    }
+    public override void behaviorAtGameTick(GameTime time)
+    {
+        if (this.targettedFarmer == null || this.targettedFarmer.currentLocation != base.currentLocation)
+        {
+            this.targettedFarmer = null;
+            this.targettedFarmer = this.findPlayer();
+        }
+        if (this.stateTimer > 0f)
+        {
+            this.stateTimer -= (float)time.ElapsedGameTime.TotalSeconds;
+            if (this.stateTimer <= 0f)
+            {
+                this.stateTimer = 0f;
+            }
+        }
+        switch (this.currentState.Value)
+        {
+            case State.Walking:
+                this.walkSpeed = 2;
+                if (this.stateTimer == 0f)
+                {
+                    this.currentState.Value = State.Walking;
+                    this.stateTimer = 1f;
+                }
+                break;
+            case State.Running:
+                this.walkSpeed = 3;
+                if (this.stateTimer == 0f)
+                {
+                    if (this.TargetInRange())
+                    {
+                        this.currentState.Value = State.Running;
+                        this.stateTimer = 1f;
+                        this.walkSpeed = 4;
+                    }
+                    else
+                    {
+                        this.currentState.Value = State.Running;
+                        this.stateTimer = 1f;
+                    }
+                }
+                break;
+            case State.Sprinting:
+                this.walkSpeed = 4;
+                if (this.stateTimer == 0f)
+                {
+                    if (this.TargetInRange())
+                    {
+                        this.currentState.Value = State.Sprinting;
+                        this.stateTimer = 2;
+                        this.walkSpeed = 5;
+                    }
+                    else
+                    {
+                        this.currentState.Value = State.Sprinting;
+                        this.stateTimer = 1f;
+                    }
+                }
+                break;
+        }
+        if (this.targettedFarmer != null && this.approachFarmer)
+        {
+            Point curTile = base.TilePoint;
+            Point playerTile = this.targettedFarmer.TilePoint;
+            if (curTile.X > playerTile.X)
+            {
+                this.velocity.X = -1f;
+            }
+            else if (curTile.X < playerTile.X)
+            {
+                this.velocity.X = 1f;
+            }
+            if (curTile.Y > playerTile.Y)
+            {
+                this.velocity.Y = -1f;
+            }
+            else if (curTile.Y < playerTile.Y)
+            {
+                this.velocity.Y = 1f;
+            }
+        }
+        if (this.velocity.X != 0f || this.velocity.Y != 0f)
+        {
+            Rectangle next_bounds = this.GetBoundingBox();
+            Vector2 next_position = base.Position;
+            next_bounds.Inflate(48, 48);
+            next_bounds.X += (int)this.velocity.X * this.walkSpeed;
+            next_position.X += (int)this.velocity.X * this.walkSpeed;
+            if (!this.TargetInRange())
+            {
+                this.velocity.X *= 1f;
+                next_bounds.X += (int)this.velocity.X * this.walkSpeed;
+                next_position.X += (int)this.velocity.X * this.walkSpeed;
+            }
+            next_bounds.Y += (int)this.velocity.Y * this.walkSpeed;
+            next_position.Y += (int)this.velocity.Y * this.walkSpeed;
+            if (!this.TargetInRange())
+            {
+                this.velocity.Y *= 1f;
+                next_bounds.Y += (int)this.velocity.Y * this.walkSpeed;
+                next_position.Y += (int)this.velocity.Y * this.walkSpeed;
+            }
+            if (base.Position != next_position)
+            {
+                base.Position = next_position;
+            }
+        }
+    }
+    public override void updateMovement(GameLocation location, GameTime time)
+    {
     }
 }
